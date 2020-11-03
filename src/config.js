d04dae0a50efc55d9891e5278e3060c5d6280516
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-11-03 11:26:17 -0500

    Implement basic functionality

---
+++ src/config.js
@@ -0,0 +1,65 @@
+const { cosmiconfigSync } = require("cosmiconfig");
+
+const debug = require("debug");
+const log = debug("live-counting-bridge:config");
+
+/**
+ * Transforms a config.
+ * @param {Object} result The config result from cosmiconfig.
+ * @returns {Object} The result with the transformed config.
+ */
+function transformConfig(result) {
+	if (!result) {
+		result = {};
+	}
+
+	const newConfig = {
+		apiBase: "",
+		threads: [],
+		token: "",
+		...(result && result.config || {}),
+	};
+
+	result.config = newConfig;
+	return result;
+}
+
+/**
+ * Gets the user-defined configuration for live-counting-bridge with defaults.
+ * @returns {Object} The configuration object.
+ */
+function getConfig() {
+	const explorer = cosmiconfigSync("live-counting-bridge", {
+		searchPlaces: [
+			"package.json",
+			"config.json",
+			".live-counting-bridgerc",
+			".live-counting-bridgerc.json",
+			".live-counting-bridgerc.yaml",
+			".live-counting-bridgerc.yml",
+			".live-counting-bridgerc.js",
+			"live-counting-bridge.config.js",
+		],
+		transform: transformConfig,
+	});
+
+	const result = explorer.search();
+	
+	if (result.filepath) {
+		log("loaded configuration from '%s'", result.filepath);
+	} else {
+		log("could not find existing configuration, using default");
+	}
+	log("loaded configuration: %O", result.config);
+
+	if (typeof result.config.apiBase !== "string" || result.config.apiBase === "") {
+		log("the matterbridge api base must be provided");
+		process.exit(1);
+	} else if (!Array.isArray(result.config.threads)) {
+		log("the threads must be an array of arrays specifying thread id and matterbridge gateway");
+		process.exit(1);
+	}
+
+	return result.config;
+}
+module.exports = getConfig;
---
+++ src/index.js
@@ -0,0 +1,132 @@
+const debug = require("debug");
+/**
+ * @type {got.Got}
+ */
+const baseGot = require("got");
+const ws = require("ws");
+
+const { version } = require("../package.json");
+const got = baseGot.extend({
+	headers: {
+		"User-Agent": "Live Counting Bridge v" + version,
+		"Content-Type": "application/json",
+	},
+	responseType: "json",
+});
+
+const config = require("./config.js")();
+
+class Thread {
+	constructor(id, gateway = "livecounting") {
+		this.id = id;
+		this.gateway = gateway;
+
+		this.log = debug("live-counting-bridge:thread:" + id);
+
+		/**
+		 * @type {ws}
+		 */
+		this.websocket = null;
+
+		this.onThreadInfo = this.onThreadInfo.bind(this);
+		this.onOpen = this.onOpen.bind(this);
+		this.onMessage = this.onMessage.bind(this);
+		this.onClose = this.onClose.bind(this);
+	}
+
+	getAboutUrl() {
+		return "https://www.reddit.com/live/" + this.id + "/about.json";
+	}
+
+	connect() {
+		const aboutUrl = this.getAboutUrl();
+		this.log("requesting thread info from %s", aboutUrl);
+		
+		got(aboutUrl, {
+			throwHttpErrors: false,
+		}).then(this.onThreadInfo).catch(error => {
+			this.log("failed to get thread info: %O", error);
+		});
+	}
+
+	/**
+	 * Handles the thread info response.
+	 * @param {got.Response} response The response.
+	 */
+	onThreadInfo(response) {
+		if (response.body.error === 404) {
+			this.log("could not find thread with id: %s", this.id);
+		} else if (response.body.error && response.body.message) {
+			this.log("failed to get thread info (error %s, message: '%s')", response.body.error, response.body.message);
+		} else if (response.body.error) {
+			this.log("failed to get thread info (error %s)", response.body.error);
+		} else if (response.body && response.body.data) {
+			const websocketUrl = response.body.data.websocket_url;
+			this.log("found websocket for thread '%s': %s", response.body.data.title, websocketUrl);
+
+			this.websocket = new ws(websocketUrl);
+			this.websocket.on("open", this.onOpen);
+			this.websocket.on("message", this.onMessage);
+			this.websocket.on("close", this.onClose);
+		}
+	}
+
+	/**
+	 * Handles a websocket open event.
+	 */
+	onOpen() {
+		this.log("websocket opened");
+	}
+
+	/**
+	 * Handles a websocket message event.
+	 * @param {string} data
+	 */
+	onMessage(data) {
+		const json = JSON.parse(data);
+		if (json.type === "update" && json.payload.data.body) {
+			this.log("handling websocket message from author %s: '%s'", json.payload.data.author, json.payload.data.body);
+
+			got.post(config.apiBase + "/api/message", {
+				headers: {
+					"Authorization": config.token && "Bearer " + config.token,
+				},
+				json: {
+					gateway: this.gateway,
+					text: json.payload.data.body,
+					username: json.payload.data.author,
+				},
+			}).then(() => {
+				this.log("sent message to bridge");
+			}).catch(error => {
+				if (error.response.statusCode) {
+					if (error.response.statusCode === 401) {
+						this.log("bridge request was unauthorized, was a valid token supplied?");
+					} else if (error.response.body.message) {
+						this.log("failed to send message to bridge (error %s, message: '%s')", error.response.statusCode, error.response.body.message);
+					} else {
+						this.log("failed to send message to bridge (error %s)", error.response.statusCode);
+					}
+				} else {
+					this.log("failed to send message to bridge: %O", error);
+				}
+			});
+		}
+	}
+
+	/**
+	 * Handles a websocket close event.
+	 * @param {ws.CloseEvent} event The close event.
+	 */
+	onClose(event) {
+		if (event.reason) {
+			this.log("reconnecting to websocket as it was closed for reason: %s", event.reason);
+		} else {
+			this.log("reconnecting to websocket");
+		}
+		this.connect();
+	}
+}
+
+const threads = config.threads.map(thread => new Thread(thread[0], thread[1]));
+threads.forEach(thread => thread.connect());
